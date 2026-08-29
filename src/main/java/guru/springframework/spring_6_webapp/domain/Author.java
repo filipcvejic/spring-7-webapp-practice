809bc0ca358ef7ca2f5a771293447e2c6526f378
filipcvejic <l3g4l.05@gmail.com>
2026-08-30 01:31:09 +0200

    adding example of repositories

--- src/main/java/guru/springframework/spring_6_webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring_6_webapp/domain/Author.java
@@ -3,6 +3,7 @@ package guru.springframework.spring_6_webapp.domain;
 import jakarta.persistence.*;
 
 import java.util.Objects;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -15,7 +16,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Set<Book> getBooks() {
         return books;
